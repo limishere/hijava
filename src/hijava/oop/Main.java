fe3bdd433f841946a1bd2c7af9bc609a49237668
lim-hj <hhhh9112@naver.com>
2023-10-19 16:53:43 +0900

    main

--- src/hijava/oop/Main.java
+++ src/hijava/oop/Main.java
@@ -1,12 +1,15 @@
 package hijava.oop;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import hijava.practice.Man;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, IOException {
 		
 //		netsport();
 //		animal();
@@ -36,11 +39,135 @@ public class Main {
 //		System.out.println(son instanceof Son); //true
 		
 		
-		software();
+//		software();
+		
+//		calulator();
+//		testinterface();
+//		total();
+//		calcoper();
+		
+//		scanner();
+		scanner2();
+		
+		
 		
 	}
 	
 	
+	private static void scanner2() {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) { //무한루프 돌릴 때는 while(true)
+			String ret = inputScan(scanner, "계산하시겠어요?(계산:Enter, 종료:quit)");
+			
+			if("quit".equalsIgnoreCase(ret)) { //대문자도 비교해줌
+				break;
+			}
+			
+			int x = inputScanNumber(scanner, "첫번째 숫자(x)를 입력하세요>>");
+			int y = inputScanNumber(scanner, "두번째 숫자(y)를 입력하세요>>");
+			
+			scanner.nextLine(); // 개행문자 제거
+			
+//			System.out.print("연산자(* 또는 /)를 입력하세요>>");
+			String op = inputScan(scanner, "연산자(* 또는 /)를 입력하세요>>");
+			
+			CalcOper co = new CalcOper();
+			
+			System.out.print(x + " " + op + " " + y + " = ");
+			
+			if("*".equals(op)) { //String비교를 할때는 ==가 아닌 equals를 사용한다!
+				co.mul(x, y);
+				
+			}else if("/".equals(op)) {
+				co.div(x, y);
+				
+			}else {
+				System.out.println("연산자를 다시 입력하세요!");
+			}
+			
+		}
+		
+		scanner.close();
+		
+	}
+	
+	//숫자받기
+	private static int inputScanNumber(Scanner scanner, String msg) {
+		System.out.print(msg);
+		return scanner.nextInt();
+	}
+	//연산자받기
+	private static String inputScan(Scanner scanner, String msg) {
+		System.out.print(msg);
+		return scanner.nextLine();
+	}
+
+
+	private static void scanner() {
+		System.out.print("문자열을 입력하세요>>");
+		Scanner scanner = new Scanner(System.in);
+		String msg = scanner.nextLine();
+		System.out.println("msg= " + msg);
+		
+		System.out.print("숫자를 입력하세요>> ");
+		double inputNum = scanner.nextDouble();
+		System.out.println("Input Number is " + inputNum);
+		
+		scanner.close();
+	}
+
+
+	private static void calcoper() {
+		int x = 10, y = 5;
+		
+		CalcOper op = new CalcOper();
+		int a = op.add(x, y);
+		int s = op.sub(x, y);
+		System.out.println(a + ", " + s);
+		
+		op.mul(x, y);
+		op.div(x, y); 
+	}
+
+
+	private static void total() {
+		int[] arr = new int[] {1, 2, 5, 9};
+		SubTotal st = new SubTotal();
+		System.out.println("Total = " + st.sum(arr));
+	}
+
+
+	private static void testinterface() throws SQLException, IOException {
+		TestInterface ti = new TestImpl();
+		
+		ti.select("select * from Table");
+		
+//		ti.out(100); ->에러
+		TestInterface.out(100);
+		//직접 불러야한다(클래스이름을 바로 부른다)
+		//static메소드는 heap영역이 아닌 정의부분(method area)에 생기기 때문에,
+		//바로 클래스이름을 쓰고 불러야 한다.
+		
+		Dog.eat("Meat"); //eat메소드는 static이라서 new하지 않아도 바로 부를 수 있다.
+//		Dog.bark(); ->에러. bark()는 heap영역에 존재하기 때문에 바로 부를 수 없다.
+	}
+
+
+	private static void calulator() {
+		Calculator calc = new CalculatorImpl(); 
+		//인터페이스는 new 불가! 
+		//추상클래스와 마찬가지로 정의만 있고 실행부분이 없기 때문에
+		//new Calculator(); 하면 에러가 난다.
+		
+		int x = 10;
+		int y = 5;
+		calc.add(x, y);
+		calc.div(x, y);
+		calc.div(x, 0);
+	}
+
+
 	private static void software() {
 		
 		SoftWare site = new WebSite();
