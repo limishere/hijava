214649b5351c9c31c30da9d59ecd59b0e3a7045e
lim-hj <hhhh9112@naver.com>
2023-10-24 23:04:27 +0900

    toString

--- src/hijava/oop/Dog.java
+++ src/hijava/oop/Dog.java
@@ -11,5 +11,21 @@ public class Dog extends Animal{
 		System.out.println("Dog eats " + food);
 	}
 	
+	//toString 오버라이드
+	//오른쪽마우스>source>generate toString으로도 작성가능(기본적인걸 만들어줌)
+	@Override
+	public String toString() {
+		return "This is dog";
+	}
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		System.out.println(d); 
+		//d자리는 String이 오는 자리기 때문에 자동으로 toString을 부름
+		
+		String s = "Test : " + d;
+		System.out.println(s);
+		//에러안남. d는 String이므로 String타입과 연산 시 문제없음
+	}
 	
 }
